d8ad8d03de537e8376303d16b51e7ad01463f8c6
Deep Pimpale <55679683+dsp0205@users.noreply.github.com>
2022-04-29 17:55:33 +0530

    Update databaseconnection.php

--- databaseconnection.php
+++ databaseconnection.php
@@ -1,5 +1,5 @@
 <?php
 // Create connection
-$con=mysqli_connect("localhost","root","","onlineauction");
+$con=mysqli_connect("127.0.0.1","root","","onlineauction");
 echo mysqli_connect_error();
 ?>
